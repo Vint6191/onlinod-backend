f6aead75eca6b5a0d26ea1c82b304612f5dc7552
Vint6191 <134882345+Vint6191@users.noreply.github.com>
2026-05-01 13:57:14 +0300

    Delete src directory

--- src/middleware/admin.js
+++
@@ -1,7 +0,0 @@
-const { adminSessionRequired } = require("./admin-session");
-
-function adminRequired(req, res, next) {
-  return adminSessionRequired(req, res, next);
-}
-
-module.exports = { adminRequired };
